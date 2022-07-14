da85579ba09d25ce520e24cc2d34eb93eb78a814
ChristabelKindrat <christabelkril@gmail.com>
2022-07-14 15:33:54 +0300

    hw4.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useForm} from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+
+    const {register, handleSubmit} = useForm()
+
+    let submit = (obj) => {
+
+        fetch("http://jsonplaceholder.typicode.com/users", {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(obj),
+        })
+            .then(response => response.json())
+            .then(result => console.log(result));
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(submit)}>
+                <input type='text' {...register('title')}/>
+                <input type='text' {...register('body')} />
+                <button>save</button>
+            </form>
+        </div>
+    );
 }
-
-export default App;
